7e36b8a4e10a03bc106789c23c68f829f79af1af
Marcel Suter <marcel.suter@bzz.ch>
2023-05-31 17:09:39 +0200

    added support for array-data

--- formdata/index.php
+++ formdata/index.php
@@ -1,25 +1,39 @@
 <!DOCTYPE html>
 <html lang='de'>
 <head>
-    <meta charset='UTF-8'>
-    <title>Formulardaten</title>
+  <meta charset='UTF-8'>
+  <title>Formulardaten</title>
 </head>
 <body>
-  <table>
-<?php
-/**
- * demo script that shows the form-data the client sent
- */
+<table>
+    <?php
+    /**
+     * demo script that shows the form-data the client sent
+     */
 
-  $method = $_SERVER['REQUEST_METHOD'];
-  echo "<tr><td colspan='2'>HTTP-Methode:</td><td>$method</td>";
-  if ($method == 'POST' || $method == 'PUT') {
-      echo "<tr><td colspan='3'>Eingabefelder:</td>";
-      foreach($_POST as $key => $value) {
-          echo "<tr><td>&nbsp;</td><td>$key</td><td>=</td><td>$value</td></tr>";
-      }
-  }
-?>
-    </table>
-  </body>
+    $method = $_SERVER['REQUEST_METHOD'];
+    echo "<tr><td colspan='2'>HTTP-Methode:</td><td>$method</td>";
+    if ($method == 'POST' || $method == 'PUT') {
+        echo "<tr><td colspan='3'>Eingabefelder:</td>";
+        foreach ($_POST as $key => $value) {
+            if ($value)
+                showValue($key, $value);
+        }
+    }
+
+    function showValue($key, $value)
+    {
+        if (is_array($value)) {
+            foreach ($value as $id => $item) {
+                $part_key = $key . "[" . $id . "]";
+                showValue($part_key, $item);
+            }
+        } else {
+            echo "<tr><td>&nbsp;</td><td>$key</td><td>=</td><td>$value</td></tr>";
+        }
+    }
+
+    ?>
+</table>
+</body>
 </html>
